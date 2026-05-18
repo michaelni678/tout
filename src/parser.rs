9c641d9cd18fe42ee8199b5ff090c1f9c38fcf06
Michael Ni <michael.ni678@gmail.com>
2026-05-17 23:46:47 -0400

    style: reorder the parser methods so that theyre grouped based on purpose

--- src/parser.rs
+++ src/parser.rs
@@ -148,6 +148,70 @@ impl Parser {
         self.tokens.pop_front()
     }
 
+    /// Takes the next token if it is a group. Returns [`None`] if the next
+    /// token is not a group or there are no more tokens.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use quote::quote;
+    /// # use tout::assert::{assert_group_eq, assert_stream_eq};
+    /// # use tout::quasi::group;
+    /// use tout::parser::Parser;
+    ///
+    /// let mut parser = Parser::new(quote! { (self.function)(); });
+    ///
+    /// assert_group_eq!(parser.next_group().unwrap(), group! { (self.function) });
+    /// assert_group_eq!(parser.next_group().unwrap(), group! { () });
+    /// assert!(parser.next_group().is_none());
+    /// assert_stream_eq!(parser.next_trees().collect(), quote! { ; });
+    /// ```
+    pub fn next_group(&mut self) -> Option<Group> {
+        self.next_tree_if_map(TokenTree::into_group)
+    }
+
+    /// Takes the next token if it is an ident. Returns [`None`] if the next
+    /// token is not an ident or there are no more tokens.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use quote::quote;
+    /// # use tout::assert::{assert_ident_eq, assert_stream_eq};
+    /// # use tout::quasi::ident;
+    /// use tout::parser::Parser;
+    ///
+    /// let mut parser = Parser::new(quote! { let x = 5; });
+    ///
+    /// assert_ident_eq!(parser.next_ident().unwrap(), ident! { let });
+    /// assert_ident_eq!(parser.next_ident().unwrap(), ident! { x });
+    /// assert!(parser.next_ident().is_none());
+    /// assert_stream_eq!(parser.next_trees().collect(), quote! { = 5; });
+    /// ```
+    pub fn next_ident(&mut self) -> Option<Ident> {
+        self.next_tree_if_map(TokenTree::into_ident)
+    }
+
+    /// Takes the next token if it is a punct. Returns [`None`] if the next
+    /// token is not a punct or there are no more tokens.
+    ///
+    /// # Examples
+    ///
+    /// See [`Parser::next_group`] and [`Parser::next_ident`].
+    pub fn next_punct(&mut self) -> Option<Punct> {
+        self.next_tree_if_map(TokenTree::into_punct)
+    }
+
+    /// Takes the next token if it is a literal. Returns [`None`] if the next
+    /// token is not a literal or there are no more tokens.
+    ///
+    /// # Examples
+    ///
+    /// See [`Parser::next_group`] and [`Parser::next_ident`].
+    pub fn next_literal(&mut self) -> Option<Literal> {
+        self.next_tree_if_map(TokenTree::into_literal)
+    }
+
     /// Takes the next tree and applies the function `map` to it. If the closure
     /// returns [`Ok`], the result is returned. Otherwise, the value is put back
     /// for the next iteration.
@@ -162,6 +226,25 @@ impl Parser {
                 None
             }
         }
+    }
+
+    /// Takes the next tree and applies the function `map` to it. If the closure
+    /// returns [`Ok`] and the predicate returns `true`, the value is returned.
+    /// Otherwise, the value is put back for the next iteration.
+    pub fn next_tree_if_map_and<T, M, P>(&mut self, map: M, predicate: P) -> Option<T>
+    where
+        T: Into<TokenTree>,
+        M: FnOnce(TokenTree) -> Result<T, TokenTree>,
+        P: FnOnce(&T) -> bool,
+    {
+        let other = match self.next_tree().map(map)? {
+            Ok(token) if predicate(&token) => return Some(token),
+            Ok(token) => token.into(),
+            Err(other) => other,
+        };
+
+        self.tokens.push_front(other);
+        None
     }
 
     /// Takes the next tree if it matches the given predicate. Returns [`None`]
@@ -192,99 +275,6 @@ impl Parser {
         P: FnOnce(&TokenTree) -> bool,
     {
         self.tokens.pop_front_if(|token| predicate(token))
-    }
-
-    /// Takes the next tree and applies the function `map` to it. If the closure
-    /// returns [`Ok`] and the predicate returns `true`, the value is returned.
-    /// Otherwise, the value is put back for the next iteration.
-    pub fn next_tree_if_map_and<T, M, P>(&mut self, map: M, predicate: P) -> Option<T>
-    where
-        T: Into<TokenTree>,
-        M: FnOnce(TokenTree) -> Result<T, TokenTree>,
-        P: FnOnce(&T) -> bool,
-    {
-        let other = match self.next_tree().map(map)? {
-            Ok(token) if predicate(&token) => return Some(token),
-            Ok(token) => token.into(),
-            Err(other) => other,
-        };
-
-        self.tokens.push_front(other);
-        None
-    }
-
-    /// Returns an iterator over the rest of the tokens.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// # use proc_macro2::TokenTree;
-    /// # use quote::quote;
-    /// # use tout::assert::{assert_stream_eq, assert_tree_eq};
-    /// # use tout::quasi::tree;
-    /// use tout::parser::Parser;
-    ///
-    /// let mut parser = Parser::new(quote! { number < 10 });
-    ///
-    /// let trees: Vec<TokenTree> = parser.next_trees().take(2).collect();
-    ///
-    /// assert_tree_eq!(trees[0], tree! { number });
-    /// assert_tree_eq!(trees[1], tree! { < });
-    /// assert_stream_eq!(parser.next_trees().collect(), quote! { 10 });
-    /// ```
-    pub fn next_trees(&mut self) -> impl Iterator<Item = TokenTree> {
-        iter::from_fn(|| self.tokens.pop_front())
-    }
-
-    /// Returns an iterator over consecutive trees that satisfy the given
-    /// predicate.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// # use proc_macro2::TokenTree;
-    /// # use quote::quote;
-    /// # use tout::assert::{assert_stream_eq, assert_tree_eq};
-    /// # use tout::quasi::tree;
-    /// use tout::parser::Parser;
-    ///
-    /// let mut parser = Parser::new(quote! { number < 10 });
-    ///
-    /// let trees: Vec<TokenTree> = parser
-    ///     .next_trees_while(|tree| matches!(tree, TokenTree::Ident(_) | TokenTree::Punct(_)))
-    ///     .collect();
-    ///
-    /// assert_tree_eq!(trees[0], tree! { number });
-    /// assert_tree_eq!(trees[1], tree! { < });
-    /// assert_stream_eq!(parser.next_trees().collect(), quote! { 10 });
-    /// ```
-    pub fn next_trees_while<P>(&mut self, mut predicate: P) -> impl Iterator<Item = TokenTree>
-    where
-        P: FnMut(&TokenTree) -> bool,
-    {
-        iter::from_fn(move || self.next_tree_if(&mut predicate))
-    }
-
-    /// Takes the next token if it is a group. Returns [`None`] if the next
-    /// token is not a group or there are no more tokens.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// # use quote::quote;
-    /// # use tout::assert::{assert_group_eq, assert_stream_eq};
-    /// # use tout::quasi::group;
-    /// use tout::parser::Parser;
-    ///
-    /// let mut parser = Parser::new(quote! { (self.function)(); });
-    ///
-    /// assert_group_eq!(parser.next_group().unwrap(), group! { (self.function) });
-    /// assert_group_eq!(parser.next_group().unwrap(), group! { () });
-    /// assert!(parser.next_group().is_none());
-    /// assert_stream_eq!(parser.next_trees().collect(), quote! { ; });
-    /// ```
-    pub fn next_group(&mut self) -> Option<Group> {
-        self.next_tree_if_map(TokenTree::into_group)
     }
 
     /// Takes the next token if it is a group and it matches the given
@@ -319,6 +309,87 @@ impl Parser {
         self.next_tree_if_map_and(TokenTree::into_group, predicate)
     }
 
+    /// Takes the next token if it is an ident and it matches the given
+    /// predicate. Returns [`None`] if the next token is not an ident, if the
+    /// predicate returns `false`, or if there are no more tokens.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use quote::quote;
+    /// # use tout::assert::{assert_ident_eq, assert_stream_eq};
+    /// # use tout::quasi::ident;
+    /// use tout::parser::Parser;
+    ///
+    /// let mut parser = Parser::new(quote! { let x = 5; });
+    ///
+    /// let ident = parser.next_ident_if(|ident| ident == "let");
+    /// assert_ident_eq!(ident.unwrap(), ident! { let });
+    ///
+    /// // `None` is returned because `x` isn't `y`.
+    /// let ident = parser.next_ident_if(|ident| ident == "y");
+    /// assert!(ident.is_none());
+    ///
+    /// assert_stream_eq!(parser.next_trees().collect(), quote! { x = 5; });
+    /// ```
+    pub fn next_ident_if<P>(&mut self, predicate: P) -> Option<Ident>
+    where
+        P: FnOnce(&Ident) -> bool,
+    {
+        self.next_tree_if_map_and(TokenTree::into_ident, predicate)
+    }
+
+    /// Takes the next token if it is a punct and it matches the given
+    /// predicate. Returns [`None`] if the next token is not a punct, if the
+    /// predicate returns `false`, or if there are no more tokens.
+    ///
+    /// # Examples
+    ///
+    /// See [`Parser::next_group_if`] and [`Parser::next_ident_if`].
+    pub fn next_punct_if<P>(&mut self, predicate: P) -> Option<Punct>
+    where
+        P: FnOnce(&Punct) -> bool,
+    {
+        self.next_tree_if_map_and(TokenTree::into_punct, predicate)
+    }
+
+    /// Takes the next token if it is a literal and it matches the given
+    /// predicate. Returns [`None`] if the next token is not a literal, if the
+    /// predicate returns `false`, or if there are no more tokens.
+    ///
+    /// # Examples
+    ///
+    /// See [`Parser::next_group_if`] and [`Parser::next_ident_if`].
+    pub fn next_literal_if<P>(&mut self, predicate: P) -> Option<Literal>
+    where
+        P: FnOnce(&Literal) -> bool,
+    {
+        self.next_tree_if_map_and(TokenTree::into_literal, predicate)
+    }
+
+    /// Returns an iterator over the rest of the tokens.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use proc_macro2::TokenTree;
+    /// # use quote::quote;
+    /// # use tout::assert::{assert_stream_eq, assert_tree_eq};
+    /// # use tout::quasi::tree;
+    /// use tout::parser::Parser;
+    ///
+    /// let mut parser = Parser::new(quote! { number < 10 });
+    ///
+    /// let trees: Vec<TokenTree> = parser.next_trees().take(2).collect();
+    ///
+    /// assert_tree_eq!(trees[0], tree! { number });
+    /// assert_tree_eq!(trees[1], tree! { < });
+    /// assert_stream_eq!(parser.next_trees().collect(), quote! { 10 });
+    /// ```
+    pub fn next_trees(&mut self) -> impl Iterator<Item = TokenTree> {
+        iter::from_fn(|| self.tokens.pop_front())
+    }
+
     /// Returns an iterator over consecutive groups.
     ///
     /// # Examples
@@ -340,6 +411,76 @@ impl Parser {
     /// ```
     pub fn next_groups(&mut self) -> impl Iterator<Item = Group> {
         iter::from_fn(|| self.next_group())
+    }
+
+    /// Returns an iterator over consecutive idents.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use proc_macro2::Ident;
+    /// # use quote::quote;
+    /// # use tout::assert::{assert_ident_eq, assert_stream_eq};
+    /// # use tout::quasi::ident;
+    /// use tout::parser::Parser;
+    ///
+    /// let mut parser = Parser::new(quote! { let x = 5; });
+    ///
+    /// let idents: Vec<Ident> = parser.next_idents().collect();
+    ///
+    /// assert_ident_eq!(idents[0], ident! { let });
+    /// assert_ident_eq!(idents[1], ident! { x });
+    /// assert_stream_eq!(parser.next_trees().collect(), quote! { = 5; });
+    /// ```
+    pub fn next_idents(&mut self) -> impl Iterator<Item = Ident> {
+        iter::from_fn(|| self.next_ident())
+    }
+
+    /// Returns an iterator over consecutive puncts.
+    ///
+    /// # Examples
+    ///
+    /// See [`Parser::next_groups`] and [`Parser::next_idents`].
+    pub fn next_puncts(&mut self) -> impl Iterator<Item = Punct> {
+        iter::from_fn(|| self.next_punct())
+    }
+
+    /// Returns an iterator over consecutive literals.
+    ///
+    /// # Examples
+    ///
+    /// See [`Parser::next_groups`] and [`Parser::next_idents`].
+    pub fn next_literals(&mut self) -> impl Iterator<Item = Literal> {
+        iter::from_fn(|| self.next_literal())
+    }
+
+    /// Returns an iterator over consecutive trees that satisfy the given
+    /// predicate.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use proc_macro2::TokenTree;
+    /// # use quote::quote;
+    /// # use tout::assert::{assert_stream_eq, assert_tree_eq};
+    /// # use tout::quasi::tree;
+    /// use tout::parser::Parser;
+    ///
+    /// let mut parser = Parser::new(quote! { number < 10 });
+    ///
+    /// let trees: Vec<TokenTree> = parser
+    ///     .next_trees_while(|tree| matches!(tree, TokenTree::Ident(_) | TokenTree::Punct(_)))
+    ///     .collect();
+    ///
+    /// assert_tree_eq!(trees[0], tree! { number });
+    /// assert_tree_eq!(trees[1], tree! { < });
+    /// assert_stream_eq!(parser.next_trees().collect(), quote! { 10 });
+    /// ```
+    pub fn next_trees_while<P>(&mut self, mut predicate: P) -> impl Iterator<Item = TokenTree>
+    where
+        P: FnMut(&TokenTree) -> bool,
+    {
+        iter::from_fn(move || self.next_tree_if(&mut predicate))
     }
 
     /// Returns an iterator over consecutive groups that satisfy the given
@@ -371,81 +512,6 @@ impl Parser {
         iter::from_fn(move || self.next_group_if(&mut predicate))
     }
 
-    /// Takes the next token if it is an ident. Returns [`None`] if the next
-    /// token is not an ident or there are no more tokens.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// # use quote::quote;
-    /// # use tout::assert::{assert_ident_eq, assert_stream_eq};
-    /// # use tout::quasi::ident;
-    /// use tout::parser::Parser;
-    ///
-    /// let mut parser = Parser::new(quote! { let x = 5; });
-    ///
-    /// assert_ident_eq!(parser.next_ident().unwrap(), ident! { let });
-    /// assert_ident_eq!(parser.next_ident().unwrap(), ident! { x });
-    /// assert!(parser.next_ident().is_none());
-    /// assert_stream_eq!(parser.next_trees().collect(), quote! { = 5; });
-    /// ```
-    pub fn next_ident(&mut self) -> Option<Ident> {
-        self.next_tree_if_map(TokenTree::into_ident)
-    }
-
-    /// Takes the next token if it is an ident and it matches the given
-    /// predicate. Returns [`None`] if the next token is not an ident, if the
-    /// predicate returns `false`, or if there are no more tokens.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// # use quote::quote;
-    /// # use tout::assert::{assert_ident_eq, assert_stream_eq};
-    /// # use tout::quasi::ident;
-    /// use tout::parser::Parser;
-    ///
-    /// let mut parser = Parser::new(quote! { let x = 5; });
-    ///
-    /// let ident = parser.next_ident_if(|ident| ident == "let");
-    /// assert_ident_eq!(ident.unwrap(), ident! { let });
-    ///
-    /// // `None` is returned because `x` isn't `y`.
-    /// let ident = parser.next_ident_if(|ident| ident == "y");
-    /// assert!(ident.is_none());
-    ///
-    /// assert_stream_eq!(parser.next_trees().collect(), quote! { x = 5; });
-    /// ```
-    pub fn next_ident_if<P>(&mut self, predicate: P) -> Option<Ident>
-    where
-        P: FnOnce(&Ident) -> bool,
-    {
-        self.next_tree_if_map_and(TokenTree::into_ident, predicate)
-    }
-
-    /// Returns an iterator over consecutive idents.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// # use proc_macro2::Ident;
-    /// # use quote::quote;
-    /// # use tout::assert::{assert_ident_eq, assert_stream_eq};
-    /// # use tout::quasi::ident;
-    /// use tout::parser::Parser;
-    ///
-    /// let mut parser = Parser::new(quote! { let x = 5; });
-    ///
-    /// let idents: Vec<Ident> = parser.next_idents().collect();
-    ///
-    /// assert_ident_eq!(idents[0], ident! { let });
-    /// assert_ident_eq!(idents[1], ident! { x });
-    /// assert_stream_eq!(parser.next_trees().collect(), quote! { = 5; });
-    /// ```
-    pub fn next_idents(&mut self) -> impl Iterator<Item = Ident> {
-        iter::from_fn(|| self.next_ident())
-    }
-
     /// Returns an iterator over consecutive idents that satisfy the given
     /// predicate.
     ///
@@ -472,39 +538,6 @@ impl Parser {
         iter::from_fn(move || self.next_ident_if(&mut predicate))
     }
 
-    /// Takes the next token if it is a punct. Returns [`None`] if the next
-    /// token is not a punct or there are no more tokens.
-    ///
-    /// # Examples
-    ///
-    /// See [`Parser::next_group`] and [`Parser::next_ident`].
-    pub fn next_punct(&mut self) -> Option<Punct> {
-        self.next_tree_if_map(TokenTree::into_punct)
-    }
-
-    /// Takes the next token if it is a punct and it matches the given
-    /// predicate. Returns [`None`] if the next token is not a punct, if the
-    /// predicate returns `false`, or if there are no more tokens.
-    ///
-    /// # Examples
-    ///
-    /// See [`Parser::next_group_if`] and [`Parser::next_ident_if`].
-    pub fn next_punct_if<P>(&mut self, predicate: P) -> Option<Punct>
-    where
-        P: FnOnce(&Punct) -> bool,
-    {
-        self.next_tree_if_map_and(TokenTree::into_punct, predicate)
-    }
-
-    /// Returns an iterator over consecutive puncts.
-    ///
-    /// # Examples
-    ///
-    /// See [`Parser::next_groups`] and [`Parser::next_idents`].
-    pub fn next_puncts(&mut self) -> impl Iterator<Item = Punct> {
-        iter::from_fn(|| self.next_punct())
-    }
-
     /// Returns an iterator over consecutive puncts that satisfy the given
     /// predicate.
     ///
@@ -516,39 +549,6 @@ impl Parser {
         P: FnMut(&Punct) -> bool,
     {
         iter::from_fn(move || self.next_punct_if(&mut predicate))
-    }
-
-    /// Takes the next token if it is a literal. Returns [`None`] if the next
-    /// token is not a literal or there are no more tokens.
-    ///
-    /// # Examples
-    ///
-    /// See [`Parser::next_group`] and [`Parser::next_ident`].
-    pub fn next_literal(&mut self) -> Option<Literal> {
-        self.next_tree_if_map(TokenTree::into_literal)
-    }
-
-    /// Takes the next token if it is a literal and it matches the given
-    /// predicate. Returns [`None`] if the next token is not a literal, if the
-    /// predicate returns `false`, or if there are no more tokens.
-    ///
-    /// # Examples
-    ///
-    /// See [`Parser::next_group_if`] and [`Parser::next_ident_if`].
-    pub fn next_literal_if<P>(&mut self, predicate: P) -> Option<Literal>
-    where
-        P: FnOnce(&Literal) -> bool,
-    {
-        self.next_tree_if_map_and(TokenTree::into_literal, predicate)
-    }
-
-    /// Returns an iterator over consecutive literals.
-    ///
-    /// # Examples
-    ///
-    /// See [`Parser::next_groups`] and [`Parser::next_idents`].
-    pub fn next_literals(&mut self) -> impl Iterator<Item = Literal> {
-        iter::from_fn(|| self.next_literal())
     }
 
     /// Returns an iterator over consecutive literals that satisfy the given
